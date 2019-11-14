b60432e72f09e1adc44eefc7c1dcd516989cec84
BonitaNjapa <Ntembekonjapa@hotmail.com>
2019-11-14 12:41:38 +0200

    Add files via upload

---
+++ algorithm/algorithm/Program.cs
@@ -0,0 +1,103 @@
+﻿
+using System;
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = 0;
+            do
+            {
+                userInput = DisplayMenu();
+                switch (userInput)
+                {
+                    case 1:
+                        decToHex();
+                        break;
+                    case 2:
+                        palindrome();
+                        break;
+                }
+            } while (userInput != 3);
+        }
+        #region methods
+        static public int DisplayMenu()
+        {
+
+            Console.WriteLine("----Menu-----");
+            Console.WriteLine("1. dec to hex");
+            Console.WriteLine("2. Palindrome");
+            Console.WriteLine("3. Exit");
+            var result = Console.ReadLine();
+            if (String.IsNullOrEmpty(result) )
+                return DisplayMenu(); 
+            return Convert.ToInt32(result);
+        }
+        public static void decToHex()
+        {
+            int decimalNumber, quotient;
+            int i = 1, j, temp = 0;
+            char[] hexadecimalNumber = new char[100];
+            char temp1;
+            Console.WriteLine("Enter a 2 Decimal Number :");
+            decimalNumber = int.Parse(Console.ReadLine());
+            if (decimalNumber.ToString().Length == 2)
+            {
+                quotient = decimalNumber;
+                while (quotient != 0)
+                {
+                    temp = quotient % 16;
+                    if (temp < 10)
+                        temp = temp + 48;
+                    else
+                        temp = temp + 55;
+                    temp1 = Convert.ToChar(temp);
+                    hexadecimalNumber[i++] = temp1;
+                    quotient = quotient / 16;
+                }
+                Console.Write("Equivalent HexaDecimal Number is ");
+                for (j = i - 1; j > 0; j--)
+                    Console.Write(hexadecimalNumber[j]);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write("Invalid ,");
+                decToHex();
+            }
+        }
+        public static void palindrome()
+        {
+            int num, rem, sum = 0, temp;
+            Console.WriteLine("Enter a 4 digit number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num.ToString().Length == 4)
+            {
+                temp = num;
+                while (num > 0)
+                {
+                    rem = num % 10;
+                    num = num / 10;
+                    sum = sum * 10 + rem;
+
+                }
+                Console.WriteLine("The Reversed Number is: {0}", sum);
+                if (temp == sum)
+                {
+                    Console.WriteLine("Number is Palindrome ");
+                }
+                else
+                {
+                    Console.WriteLine("Number {0} is not a palindrome ",sum);
+                }
+            }
+            else
+            {
+                Console.Write("Invalid Number ,{0}",sum);
+                palindrome();
+            }
+        }
+        #endregion
+    }
+}
